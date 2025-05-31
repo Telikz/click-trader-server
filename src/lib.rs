046420073d0de6d96486b25c6322c89fe62b686b
Robin Olsen <robinolsen98@outlook.com>
2025-05-31 16:26:56 +0200

    Add player table, set_name and increace money reducer

--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,62 @@
-use spacetimedb::{ReducerContext, Table};
+use spacetimedb::sats::u256;
+use spacetimedb::{reducer, table, Identity, ReducerContext, Table};
 
-#[spacetimedb::table(name = person)]
-pub struct Person {
-    name: String
+#[table(name = player, public)]
+pub struct Player {
+    #[primary_key]
+    identity: Identity,
+    username: Option<String>,
+    money: u256,
+    click_power: u64,
+    online: bool,
 }
 
-#[spacetimedb::reducer(init)]
+#[reducer(init)]
 pub fn init(_ctx: &ReducerContext) {
-    // Called when the module is initially published
 }
 
-#[spacetimedb::reducer(client_connected)]
-pub fn identity_connected(_ctx: &ReducerContext) {
-    // Called everytime a new client connects
-}
+#[reducer(client_connected)]
+pub fn identity_connected(ctx: &ReducerContext) {
+    let identity = ctx.sender;
 
-#[spacetimedb::reducer(client_disconnected)]
-pub fn identity_disconnected(_ctx: &ReducerContext) {
-    // Called everytime a client disconnects
-}
-
-#[spacetimedb::reducer]
-pub fn add(ctx: &ReducerContext, name: String) {
-    ctx.db.person().insert(Person { name });
-}
-
-#[spacetimedb::reducer]
-pub fn say_hello(ctx: &ReducerContext) {
-    for person in ctx.db.person().iter() {
-        log::info!("Hello, {}!", person.name);
+    if ctx.db.player().identity().find(identity).is_none() {
+        ctx.db.player().insert(Player {
+            identity,
+            username: None,
+            money: u256::new(0),
+            click_power: 1,
+            online: true,
+        });
+    } else {
+        if let Some(mut player) = ctx.db.player().identity().find(identity) {
+            player.online = true;
+            ctx.db.player().identity().update(player);
+        }
     }
-    log::info!("Hello, World!");
+}
+
+#[reducer(client_disconnected)]
+pub fn identity_disconnected(_ctx: &ReducerContext) {
+}
+
+#[reducer]
+pub fn increase_money(ctx: &ReducerContext) -> Result<(), String> {
+    if let Some(mut player) = ctx.db.player().identity().find(ctx.sender) {
+        player.money += u256::from(player.click_power);
+        ctx.db.player().identity().update(player);
+        Ok(())
+    } else {
+        Err("Cannot reward bonus for unknown player".to_string())
+    }
+}
+
+
+#[reducer]
+pub fn set_name(ctx: &ReducerContext, username: String) -> Result<(), String> {
+    if let Some(player) = ctx.db.player().identity().find(ctx.sender) {
+        ctx.db.player().identity().update(Player { username: Some(username), ..player });
+        Ok(())
+    } else {
+        Err("Cannot set name for unknown user".to_string())
+    }
 }
